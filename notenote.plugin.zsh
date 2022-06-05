5d061061d5323b404740f877bd0f3767ab506dc4
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2022-06-05 10:42:48 -0600

    Update notenote.plugin.zsh

--- notenote.plugin.zsh
+++ notenote.plugin.zsh
@@ -1,6 +1,6 @@
 mkdir ~/.notenote || echo
 function notenote() {
     if [$1 = "save"] {nano ~/.notenote/$2.note}
-    else if [$1 = "view"] {cat ~/.notenote/$2.note}
-    else if [$1 = "edit"] {nano ~/.notenote/$2.note}
+    elif [$1 = "view"] {cat ~/.notenote/$2.note}
+    elif [$1 = "edit"] {nano ~/.notenote/$2.note}
 }
